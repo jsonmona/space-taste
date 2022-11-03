bbb18e87d30649e9189301bc73cf62a348221e7e
juhee <yujh0208@naver.com>
2022-11-04 01:20:51 +0900

    nav3_v1

--- app/src/main/java/cf/spacetaste/app/Nav3.java
+++ app/src/main/java/cf/spacetaste/app/Nav3.java
@@ -1,5 +1,22 @@
 package cf.spacetaste.app;
 
-public class Nav3 {
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Nav3 extends AppCompatActivity {
+    ArrayAdapter<CharSequence> adapter = null;
+    Spinner spinner = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.nav3);
+
+        adapter = ArrayAdapter.createFromResource(this, R.array.nav3Cate, android.R.layout.simple_spinner_dropdown_item);
+        spinner = findViewById(R.id.spinner);
+        spinner.setAdapter(adapter);
+    }
 }
