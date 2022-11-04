fa91e5d899a9930535562bbfa0b21a61d9197ce6
juhee <yujh0208@naver.com>
2022-11-04 16:53:44 +0900

    nav3_v1

--- app/src/main/java/cf/spacetaste/app/MainActivity.java
+++ app/src/main/java/cf/spacetaste/app/MainActivity.java
@@ -1,8 +1,15 @@
 package cf.spacetaste.app;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = (Button) findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Nav3.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
+//    private BottomNavigationView.OnNavigationItemSelectedListener OnNavigationItemSelectedListener;{
+//        OnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+//
+//            @Override
+//            public boolean setOnNavigationItemSelectedListener(@Nullable MenuItem item){
+//
+//                switch (item.getItemId()){
+//                    case R.id.navbar1:
+//                        Intent intent = new Intent(MainActivity.this, MainActivity.class);
+//                        startActivity(intent);
+//                    case R.id.navbar2:
+//                        Intent intent2 = new Intent(MainActivity.this, MainActivity.class);
+//                        startActivity(intent2);
+//                    case R.id.navbar3:
+//                        Intent intent3 = new Intent(MainActivity.this, Nav3.class);
+//                        startActivity(intent3);
+//                    case R.id.navbar4:
+//                        Intent intent4 = new Intent(MainActivity.this, MainActivity.class);
+//                        startActivity(intent4);
+//                }
+//            }
+//        };
+//    }
 }
--- app/src/main/java/cf/spacetaste/app/Nav3.java
+++ app/src/main/java/cf/spacetaste/app/Nav3.java
@@ -2,6 +2,7 @@ package cf.spacetaste.app;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.RatingBar;
 import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class Nav3 extends AppCompatActivity {
     ArrayAdapter<CharSequence> adapter = null;
     Spinner spinner = null;
+    private RatingBar ratingbar1,ratingbar2,ratingbar3,ratingbar4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +20,24 @@ public class Nav3 extends AppCompatActivity {
         adapter = ArrayAdapter.createFromResource(this, R.array.nav3Cate, android.R.layout.simple_spinner_dropdown_item);
         spinner = findViewById(R.id.spinner);
         spinner.setAdapter(adapter);
+
+        ratingbar1 = findViewById(R.id.rate1);
+        ratingbar2 = findViewById(R.id.rate2);
+        ratingbar3 = findViewById(R.id.rate3);
+        ratingbar4 = findViewById(R.id.rate4);
+
+        ratingbar1.setOnRatingBarChangeListener(new Listener());
+        ratingbar2.setOnRatingBarChangeListener(new Listener());
+        ratingbar3.setOnRatingBarChangeListener(new Listener());
+        ratingbar4.setOnRatingBarChangeListener(new Listener());
     }
 }
+
+class Listener implements RatingBar.OnRatingBarChangeListener
+{
+    @Override
+    public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+        ratingBar.setRating(rating);
+    }
+}
+
