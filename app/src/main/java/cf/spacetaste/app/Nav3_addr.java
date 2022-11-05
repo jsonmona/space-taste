3cf06c3636375f24e7135f98a5e4d6511b511d32
juhee <yujh0208@naver.com>
2022-11-05 14:39:45 +0900

    nav3_v2

---
+++ app/src/main/java/cf/spacetaste/app/Nav3_addr.java
@@ -0,0 +1,47 @@
+package cf.spacetaste.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class Nav3_addr extends AppCompatActivity {
+
+    private WebView browser;
+
+    class MyJavaScriptInterface
+    {
+        @JavascriptInterface
+        @SuppressWarnings("unused")
+        public void processDATA(String data) {
+            Bundle extra = new Bundle();
+            Intent intent = new Intent();
+            extra.putString("data", data);
+            intent.putExtras(extra);
+            setResult(RESULT_OK, intent);
+            finish();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.nav3_web_view);
+
+        browser = (WebView) findViewById(R.id.webView);
+        browser.getSettings().setJavaScriptEnabled(true);
+        browser.addJavascriptInterface(new MyJavaScriptInterface(), "Android");
+
+        browser.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                browser.loadUrl("javascript:sample2_execDaumPostcode();");
+            }
+        });
+
+        browser.loadUrl("https://api.space-taste.cf/address.html");
+    }
+}
