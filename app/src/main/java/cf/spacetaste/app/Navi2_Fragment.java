9d1610c8a547f3f7765554b9340d5497de740b79
hammsik <asbbrp@gmail.com>
2022-11-18 17:25:42 +0900

    feat(LM-13) : 검색 뷰(네비바2) 브랜치 생성

---
+++ app/src/main/java/cf/spacetaste/app/Navi2_Fragment.java
@@ -0,0 +1,24 @@
+package cf.spacetaste.app;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Navi2_Fragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.navi2_fragment, container, false);
+    }
+}
