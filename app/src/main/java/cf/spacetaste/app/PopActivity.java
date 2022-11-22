3208a7a92ba0d713991c4f05dbc11885447a57dc
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2022-11-22 20:54:37 +0900

    fix(LM-65): 동네인증 버튼 및 관련기능 수정

--- app/src/main/java/cf/spacetaste/app/PopActivity.java
+++ app/src/main/java/cf/spacetaste/app/PopActivity.java
@@ -10,34 +10,26 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class PopActivity extends AppCompatActivity {
-    Button btnYes, btnNo;
+    Button btnYes;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_pop);
-        Intent inIntent = getIntent();
 
         btnYes = (Button) findViewById(R.id.btnYes);
         btnYes.setOnClickListener(view -> {
             Intent ouIntent = new Intent(getApplicationContext(), NeighborhoodActivity.class);
-            ouIntent.putExtra("allow", true);
-            setResult(RESULT_OK, ouIntent);
-            finish();
-        });
-
-        btnNo = (Button) findViewById(R.id.btnNo);
-        btnNo.setOnClickListener(view -> {
-            Intent ouIntent = new Intent(getApplicationContext(), NeighborhoodActivity.class);
-            ouIntent.putExtra("allow", false);
             setResult(RESULT_OK, ouIntent);
             finish();
         });
     }
+
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        if( event.getAction() == MotionEvent.ACTION_OUTSIDE ) {
+        if (event.getAction() == MotionEvent.ACTION_OUTSIDE) {
             return false;
         }
         return true;
