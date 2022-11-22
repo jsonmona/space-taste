b995e0d2f6e5ceeae6c25f33121809decb8c4d27
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2022-11-22 10:08:27 +0900

    feat(LM-57): 메인페이지로 이동하는 코드 추가

--- app/src/main/java/cf/spacetaste/app/NeighborhoodActivity.java
+++ app/src/main/java/cf/spacetaste/app/NeighborhoodActivity.java
@@ -1,6 +1,7 @@
 package cf.spacetaste.app;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 
 import android.os.Bundle;
@@ -8,12 +9,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
+import com.kakao.sdk.user.UserApiClient;
+
 public class NeighborhoodActivity extends AppCompatActivity {
+    Button btnNeiCom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_neighborhood);
 
+        btnNeiCom = (Button) findViewById(R.id.btnNeiCom);
+        btnNeiCom.setOnClickListener(v -> {
+            // 거주지 저장하고
+            Intent Intent = new Intent(getApplicationContext(),HomeActivity.class);
+            startActivity(Intent); // 메인페이지로 이동
+        });
     }
 }
