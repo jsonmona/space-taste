fc542b7c5365d9b447192afd11b28e225ec747cc
변준형 <byeon@byeonjunhyeong-ui-MacBookPro.local>
2022-11-23 21:54:29 +0900

    feat(LM-8): make user info fragment

--- app/src/main/java/cf/spacetaste/app/InfoActivity.java
+++ app/src/main/java/cf/spacetaste/app/InfoActivity.java
@@ -1,26 +1,60 @@
 package cf.spacetaste.app;
 
-import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
-
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Toast;
+import android.widget.Button;
 
 import com.kakao.sdk.auth.model.OAuthToken;
 import com.kakao.sdk.common.util.Utility;
 import com.kakao.sdk.user.UserApiClient;
 
-import kotlin.Unit;
-import kotlin.jvm.functions.Function2;
-
 public class InfoActivity extends AppCompatActivity {
+
+    private Button btnRegist;
+    private Button btnLike;
+    private FragmentuserLike fragmentuserLike;
+    private FragmentuserRegist fragmentuserRegist;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+
+        fragmentuserRegist = new FragmentuserRegist();
+        fragmentuserLike = new FragmentuserLike();
+
+        //프래그먼트 매니저 획득
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        //프래그먼트 Transaction 획득
+        //프래그먼트를 올리거나 교체하는 작업을 Transaction이라고 합니다.
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        //프래그먼트를 FrameLayout의 자식으로 등록해줍니다.
+        fragmentTransaction.add(R.id.userList, fragmentuserRegist);
+        //commit을 하면 자식으로 등록된 프래그먼트가 화면에 보여집니다.
+        fragmentTransaction.commit();
+
+        btnRegist = findViewById(R.id.btnRegist);
+        btnLike = findViewById(R.id.btnLike);
+
+        btnRegist.setOnClickListener(view -> {
+            FragmentManager fm1 = getSupportFragmentManager();
+            FragmentTransaction ft1 = fragmentManager.beginTransaction();
+            ft1.replace(R.id.userList, fragmentuserRegist);
+            ft1.commit();
+        });
+
+        btnLike.setOnClickListener(view -> {
+            FragmentManager fm2 = getSupportFragmentManager();
+            FragmentTransaction ft2 = fragmentManager.beginTransaction();
+            ft2.replace(R.id.userList, fragmentuserLike);
+            ft2.commit();
+        });
+
+
     }
 }
