e18fb0130809794c631aa73a68cbc41a2b327836
jsonmona <jsonmona@outlook.com>
2022-11-23 13:19:15 +0900

    feat(LM-54): Fix backend crashing on createMatzip with no photo

--- backend/src/main/java/cf/spacetaste/backend/controller/MatzipController.java
+++ backend/src/main/java/cf/spacetaste/backend/controller/MatzipController.java
@@ -1,5 +1,6 @@
 package cf.spacetaste.backend.controller;
 
+import cf.spacetaste.backend.model.MatzipModel;
 import cf.spacetaste.backend.service.MatzipService;
 import cf.spacetaste.backend.service.PhotoService;
 import cf.spacetaste.backend.service.TokenService;
@@ -21,15 +22,11 @@ public class MatzipController {
     private final PhotoService photoService;
     private final TokenService tokenService;
 
-    @PostMapping("/matzip")
-    public MatzipInfoDTO createMatzip(@RequestHeader("authorization") String auth, @RequestBody MatzipBasicInfoDTO info) {
-        int userId = tokenService.verifyToken(auth);
-        var matzip = matzipService.createMatzip(info);
-        if (matzip == null || matzip.getMatzipId() <= 0)
-            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);
-
+    private MatzipInfoDTO queryInfo(MatzipModel matzip) {
         var hashtags = matzipService.getHashtags(matzip);
-        var photoUrl = photoService.makeUrl(photoService.getFromId(matzip.getMainPhoto()));
+        String photoUrl = null;
+        if (matzip.getMainPhoto() != null)
+            photoUrl = photoService.makeUrl(photoService.getFromId(matzip.getMainPhoto()));
 
         return new MatzipInfoDTO(
                 matzip.getMatzipId(),
@@ -39,5 +36,15 @@ public class MatzipController {
                 hashtags,
                 photoUrl
         );
+    }
+
+    @PostMapping("/matzip")
+    public MatzipInfoDTO createMatzip(@RequestHeader("authorization") String auth, @RequestBody MatzipBasicInfoDTO info) {
+        int userId = tokenService.verifyToken(auth);
+        var matzip = matzipService.createMatzip(info);
+        if (matzip == null || matzip.getMatzipId() <= 0)
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);
+
+        return queryInfo(matzip);
     }
 }
