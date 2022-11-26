593e0ee19c6f39f63a61a8df5820f484fe00d62a
jsonmona <jsonmona@outlook.com>
2022-11-26 18:09:06 +0900

    feat(LM-59): Add search api for android

--- app/src/main/java/cf/spacetaste/app/network/RetrofitService.java
+++ app/src/main/java/cf/spacetaste/app/network/RetrofitService.java
@@ -3,6 +3,7 @@ package cf.spacetaste.app.network;
 import cf.spacetaste.common.AuthResponseDTO;
 import cf.spacetaste.common.MatzipBasicInfoDTO;
 import cf.spacetaste.common.MatzipInfoDTO;
+import cf.spacetaste.common.SearchRequestDTO;
 import retrofit2.Call;
 import retrofit2.http.*;
 
@@ -18,4 +19,7 @@ public interface RetrofitService {
 
     @GET("/matzip/{matzipId}/photos")
     Call<List<String>> listMatzipPhotos(@Header("Authorization") String token, @Path("matzipId") int matzipId);
+
+    @POST("/search/v1")
+    Call<List<MatzipInfoDTO>> searchMatzip(@Header("Authorization") String token, @Body SearchRequestDTO info);
 }
