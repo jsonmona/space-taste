9e7efea66f517c5117340b63e9dcee2f210dd1c7
jsonmona <jsonmona@outlook.com>
2022-11-26 11:50:53 +0900

    feat(LM-69): Limit number of photos returned by matzip

--- backend/src/main/java/cf/spacetaste/backend/mapper/ReviewPhotoMapper.java
+++ backend/src/main/java/cf/spacetaste/backend/mapper/ReviewPhotoMapper.java
@@ -8,6 +8,6 @@ import java.util.List;
 
 @Mapper
 public interface ReviewPhotoMapper {
-    @Select("SELECT b.photo_id AS photo_id FROM review AS a INNER JOIN review_photo AS b ON a.review_id = b.review_id WHERE a.matzip_id=#{matzipId}")
+    @Select("SELECT b.photo_id AS photo_id FROM review AS a INNER JOIN review_photo AS b ON a.review_id = b.review_id WHERE a.matzip_id=#{matzipId} LIMIT 10")
     List<PhotoModel> listPhotoWithMatzip(int matzipId);
 }
