5d42003d20e48b7ad28789d08644a5cc0aec06e3
juhee <yujh0208@naver.com>
2022-12-05 10:52:56 +0900

    리뷰추가 페이지불러오기 전

--- app/src/main/java/cf/spacetaste/app/AddReview.java
+++ app/src/main/java/cf/spacetaste/app/AddReview.java
@@ -12,6 +12,7 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RatingBar;
+import android.widget.Toast;
 
 import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
@@ -23,20 +24,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+import cf.spacetaste.app.data.MatzipInfo;
+import cf.spacetaste.app.data.MatzipReview;
+import cf.spacetaste.common.ReviewInfoDTO;
 
-public class AddReview extends AppCompatActivity {
+
+public class AddReview extends AppCompatActivity{
     private Uri uri;
     private ImageView imageView;
     private ImageButton reveiwBackbtn;
-    private Button addReviewImageBtn, okButton;
+    private Button addReviewImageBtn,okButton;
     private AlertDialog.Builder builder;
     private EditText addReview_ettext;
-    private RatingBar rateTaste, ratePrice, ratePolite, rateClean;
+    private RatingBar rateTaste,ratePrice,ratePolite,rateClean;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.addreview);
+        setContentView(R.layout.activity_addreview);
 
         addReview_ettext = findViewById(R.id.addReview_ettext);
         //저장할때 불러와야하니 가져옴.
@@ -62,18 +67,70 @@ public class AddReview extends AppCompatActivity {
         rateClean.setOnRatingBarChangeListener(new Listener());
 
         reveiwBackbtn = findViewById(R.id.reveiw_backbtn);
-        reveiwBackbtn.setOnClickListener(view -> finish());
+        reveiwBackbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent( AddReview.this, Matzip_Detail.class );
+                startActivity( intent );
+            }
+        });
         okButton = findViewById(R.id.okButton);
-        okButton.setOnClickListener(view -> {
-            //DB에 추가하는 부분 추후 추가
-            builder = new AlertDialog.Builder(AddReview.this);
-            AlertDialog dialog = builder.setMessage("리뷰작성이 완료되었습니다.")
-                    .setNegativeButton("확인", (dialog1, which) -> {
-                        Intent intent = new Intent(AddReview.this, Matzip_Detail.class);
-                        startActivity(intent);
-                    })
-                    .create();
-            dialog.show();
+        okButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String detail = addReview_ettext.getText().toString();
+                int scoreTaste = rateTaste.getNumStars();
+                int scorePrice = ratePrice.getNumStars();
+                int scoreKindness = ratePolite.getNumStars();
+                int scoreClean = rateClean.getNumStars();
+                if(addReview_ettext.equals("")){
+                    AlertDialog.Builder builder = new AlertDialog.Builder(AddReview.this,R.style.MyAlertDialogStyle);
+                    AlertDialog dialog = builder.setMessage("짧은 한마디를 입력해주세요")
+                            .setNegativeButton("확인",null)
+                            .create();
+                    dialog.show();
+                } else {
+                    MatzipInfo matzip = new MatzipInfo(0,"","","",[],"",0);
+                    ReviewInfoDTO review = new ReviewInfoDTO(0,0,0, scoreTaste, scorePrice, scoreKindness, scoreClean, detail);
+                    AppState.getInstance(AddReview.this).postReview(matzip,review,((success) -> {
+                        if (success) {
+                            builder = new AlertDialog.Builder(AddReview.this);
+                            AlertDialog dialog = builder.setMessage("리뷰 작성이 완료되었습니다.")
+                                    .setNegativeButton("확인", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            Intent intent = new Intent( AddReview.this, Matzip_Detail.class );
+                                            startActivity( intent );
+                                        }
+                                    })
+
+                                    .create();
+                            dialog.show();
+                        } else {
+                            Toast.makeText(AddReview.this, "리뷰 추가에 실패하였습니다.", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                    }));
+                }
+            }
+        });
+        okButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //DB에 추가하는 부분 추후 추가
+                builder = new AlertDialog.Builder(AddReview.this);
+                AlertDialog dialog = builder.setMessage("리뷰작성이 완료되었습니다.")
+                        .setNegativeButton("확인", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Intent intent = new Intent( AddReview.this, Matzip_Detail.class );
+                                startActivity( intent );
+                            }
+                        })
+
+                        .create();
+                dialog.show();
+            }
         });
     }
 
@@ -92,14 +149,14 @@ public class AddReview extends AppCompatActivity {
             new ActivityResultCallback<ActivityResult>() {
                 @Override
                 public void onActivityResult(ActivityResult result) {
-                    if (result.getResultCode() == RESULT_OK && result.getData() != null) {
+                    if( result.getResultCode() == RESULT_OK && result.getData() != null){
                         uri = result.getData().getData();
                         try {
                             Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
                             imageView.setImageBitmap(bitmap);
-                        } catch (FileNotFoundException e) {
+                        }catch (FileNotFoundException e){
                             e.printStackTrace();
-                        } catch (IOException e) {
+                        }catch (IOException e){
                             e.printStackTrace();
                         }
                     }
