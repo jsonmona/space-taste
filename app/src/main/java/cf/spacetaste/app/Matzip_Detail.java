a2aef562bf16a9204803d0aaa739fba3e3f43a2f
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2022-12-06 23:41:58 +0900

    feat(LM-109): 이미지 리스트 크기에따른 이미지 뷰 조정

--- app/src/main/java/cf/spacetaste/app/Matzip_Detail.java
+++ app/src/main/java/cf/spacetaste/app/Matzip_Detail.java
@@ -3,15 +3,19 @@ package cf.spacetaste.app;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RatingBar;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,13 +25,14 @@ import cf.spacetaste.app.data.StarGroup;
 
 public class Matzip_Detail extends AppCompatActivity {
     private Uri uri;
-    private TextView detail_name, detail_addr, detail_ethashtag;
-    private ImageView detail_pic1, detail_pic2, detail_pic3, detail_pic4, detail_pic5, detail_pic6;
+    private TextView detail_name, detail_addr, detail_ethashtag, txtImgMore;
+    private ArrayList<ImageView> detail_pics = new ArrayList();
     private ImageButton detailBackbtn;
     private Button goReviewbtn;
     private AlertDialog.Builder builder;
     private EditText addReview_ettext;
     private RatingBar detail_rateTaste, detail_ratePrice, detail_ratePolite, detail_rateClean;
+    private TableRow secongImgRow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,25 +43,38 @@ public class Matzip_Detail extends AppCompatActivity {
         MatzipInfo matzipInfo = (MatzipInfo) inIntent.getSerializableExtra("matzipInfo");
 
         AppState.getInstance(getApplicationContext()).listMatzipReviews(matzipInfo, (success, result) -> {
-            if (result.size() != 0){
+            if (result.size() != 0) {
 
             } else {
             }
         });
 
-//        AppState.getInstance(getApplicationContext()).listMatzipPhotos(matzipInfo, (success, result) -> {
-//            if (result.size() != 0){
-//
-//            } else {
-//            }
-//        });
+        secongImgRow = findViewById(R.id.secongImgRow);
+        detail_pics.add(findViewById(R.id.detail_pic1));
+        detail_pics.add(findViewById(R.id.detail_pic2));
+        detail_pics.add(findViewById(R.id.detail_pic3));
+        detail_pics.add(findViewById(R.id.detail_pic4));
+        detail_pics.add(findViewById(R.id.detail_pic5));
+        detail_pics.add(findViewById(R.id.detail_pic6));
+        txtImgMore = findViewById(R.id.txtImgMore);
+        AppState.getInstance(getApplicationContext()).listMatzipPhotos(matzipInfo, (success, result) -> {
+            System.out.println("result: " + result);
+            if (result != null) {
+                int end_idx = 6;
+                if (result.size() < end_idx)
+                    end_idx = result.size();
+                for (int i = 0; i < end_idx; i++)
+                    Glide.with(getApplicationContext()).load(result.get(i)).into(detail_pics.get(i));
+                if (3 < result.size()) {
+                    secongImgRow.setVisibility(View.VISIBLE);
+                    if (6 < result.size()) {
+                        detail_pics.get(5).setAlpha((float) 0.4);
+                        txtImgMore.setVisibility(View.VISIBLE);
+                    }
+                }
+            }
+        });
 
-        detail_pic1 = findViewById(R.id.detail_pic1);
-        detail_pic2 = findViewById(R.id.detail_pic2);
-        detail_pic3 = findViewById(R.id.detail_pic3);
-        detail_pic4 = findViewById(R.id.detail_pic4);
-        detail_pic5 = findViewById(R.id.detail_pic5);
-        detail_pic6 = findViewById(R.id.detail_pic6);
 
         detail_name = findViewById(R.id.detail_name);
         detail_name.setText(matzipInfo.getName());
@@ -83,7 +101,7 @@ public class Matzip_Detail extends AppCompatActivity {
         goReviewbtn = findViewById(R.id.goReviewbtn);
         goReviewbtn.setOnClickListener(view -> {
             Intent intent = new Intent(Matzip_Detail.this, AddReview.class);
-            intent.putExtra("matzipInfo",matzipInfo);
+            intent.putExtra("matzipInfo", matzipInfo);
             startActivity(intent);
         });
     }
